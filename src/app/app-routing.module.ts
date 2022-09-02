f453b9b0df1db2c4a56c6f8ea81ab210769f9ba8
Ricardo Checalla Velasquez <ricardie_7@hotmail.com>
2022-09-01 22:39:59 -0500

    diseno 01

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule,Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboradComponent } from './pages/dashborad/dashborad.component';
+import { Graph1Component } from './pages/graph1/graph1.component';
+import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
+import { PagesComponent } from './pages/pages.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+
+const routes: Routes = [
+  { path: '', 
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboradComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graph1', component: Graph1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ]
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NotpagefoundComponent },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
